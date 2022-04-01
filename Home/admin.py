3a6bd1b27f15ca8bfa030ab2dee3d95c4c01fd83
AnkoorNagargoje <ankoornagargoje@gmail.com>
2022-04-01 15:58:38 +0530

    add Blog class to admin.py

--- Home/admin.py
+++ Home/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Blog
 
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'message']
 
 
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type', 'read_time']
+
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Blog, BlogAdmin)
